e5700e7a468dc6376f71819af39c6104285fc955
Tim Fish <tim@timfish.uk>
2025-06-09 19:48:21 +0200

    fix build

--- module.cc
+++ module.cc
@@ -113,9 +113,7 @@ void CaptureStackTraces(const FunctionCallbackInfo<Value> &args) {
       futures.emplace_back(std::async(
           std::launch::async,
           [thread_name, state](Isolate *isolate) -> ThreadResult {
-            return {.thread_name = thread_name,
-                    .state = state,
-                    .stack_frames = CaptureStackTrace(isolate)};
+            return ThreadResult{thread_name, state, CaptureStackTrace(isolate)};
           },
           thread_isolate));
     }
@@ -237,9 +235,7 @@ void RegisterThread(const FunctionCallbackInfo<Value> &args) {
     std::lock_guard<std::mutex> lock(threads_mutex);
     auto found = threads.find(isolate);
     if (found == threads.end()) {
-      threads.emplace(isolate, ThreadInfo{.thread_name = thread_name,
-                                          .last_seen = milliseconds::zero(),
-                                          .state = ""});
+      threads.emplace(isolate, ThreadInfo{thread_name, milliseconds::zero(), ""});
       // Register a cleanup hook to remove this thread when the isolate is
       // destroyed
       node::AddEnvironmentCleanupHook(isolate, Cleanup, isolate);
